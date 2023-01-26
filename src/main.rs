a717601e682c9689af76fb8ebe8e68939b28a8f7
Kenrick Halff <kenrickhalff@gmail.com>
2023-01-26 23:00:48 +0100

    Finished up to page 42

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+// 3.7 Working With HTML Forms
+// Page 42 - Zero to Production in Rust book
+
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    // let port = listener.local_addr().unwrap().port();
+    run(listener)?.await
 }
